e62a54f2e6dc1f11df56b632fd2de2f170050168
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-02-10 19:59:24 +0100

    created home top component

---
+++ components/HomeTop.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-container class="background">
+    <h1>Les routes du Bergerac</h1>
+  </el-container>
+</template>
+
+<style scoped>
+.el-container {
+  height: 100vh;
+}
+h1 {
+  font-family: "Milonga", cursive;
+  width: 100%;
+  text-align: center;
+  margin: auto;
+  font-size: 7vw;
+  color: white;
+}
+.background {
+  background: url("/static/images/chateaux/mobile-phone-1875813_1920.jpg");
+  background-size: cover;
+  background-position: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,17 @@
 <template>
-  <el-container>
-    <el-header>
-      <h1>Bergerac Roads</h1>
-      </el-header>
-    <el-main>
-      <el-button>Bouton standard</el-button>
-    </el-main>
-  </el-container>
+  <HomeTop />
 </template>
 
 <script>
-import AppLogo from "~/components/AppLogo.vue";
+import HomeTop from "~/components/HomeTop.vue";
 
 export default {
   components: {
-    AppLogo
+    HomeTop
   }
 };
 </script>
 
 <style>
-.el-header {
-  background-color: lightblue;
-  text-align: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
+
 </style>
