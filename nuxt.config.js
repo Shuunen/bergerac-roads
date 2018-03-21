c5edbef510b60a024abf3818e54fbef642b1b787
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-03-21 16:25:31 +0100

    added webmanifest

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,10 +9,15 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'theme-color', content: '#ffffff' },
       { hid: 'description', name: 'description', content: 'Tourisme vitivinicole dans le Bergerac' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/assets/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/assets/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/assets/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', color: '#5bbad5', href: '/assets/safari-pinned-tab.svg' },
     ]
   },
   css: [
