16dae2d5315945d36dfd2761f78fa788fc45d0af
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-03-22 10:54:23 +0100

    better pwa impl

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,13 +15,6 @@ module.exports = {
       { name: 'theme-color', content: '#ffffff' },
       { hid: 'description', name: 'description', content: 'Tourisme vitivinicole dans le Bergerac' }
     ],
-    link: [
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/assets/apple-touch-icon.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/assets/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/assets/favicon-16x16.png' },
-      { rel: 'manifest', href: '/site.webmanifest' },
-      { rel: 'mask-icon', color: '#5bbad5', href: '/assets/safari-pinned-tab.svg' },
-    ]
   },
   css: [
     '~assets/styles/shared/global.scss'
@@ -41,6 +34,7 @@ module.exports = {
     ['nuxt-sass-resources-loader', {
       resources: ['./assets/styles/ressources/variables.scss']
     }],
+    '@nuxtjs/pwa',
   ],
   build: {
     vendor: ['element-ui'],
