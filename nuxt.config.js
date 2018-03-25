e65572793e832d8c68e26b5275454ddc3684362d
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-03-25 19:28:29 +0200

    added purgecss

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,6 @@
-// const path = require('path')
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
+const path = require('path')
 
 module.exports = {
   /*
@@ -64,6 +66,19 @@ module.exports = {
           exclude: /(node_modules)/,
         })
       }
+
+      // Remove unused CSS using purgecss. See https://github.com/FullHuman/purgecss
+      // for more information about purgecss.
+      config.plugins.push(
+        new PurgecssPlugin({
+          paths: glob.sync([
+            path.join(__dirname, './pages/**/*.vue'),
+            path.join(__dirname, './layouts/**/*.vue'),
+            path.join(__dirname, './components/**/*.vue')
+          ]),
+          whitelist: ['html', 'body']
+        })
+      )
     },
   },
 }
