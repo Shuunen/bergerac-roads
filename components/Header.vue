290269ec6ceb1e445d9ef9d33bf89fd6051a826e
LeGaby <leo.gaboriau@gmail.com>
2018-05-31 20:12:40 +0200

    Added temporary menu

--- components/Header.vue
+++ components/Header.vue
@@ -3,6 +3,30 @@
     <div class="logo-wrapper">
       <Logo />
     </div>
+    <!-- Next div is temporary and will eventually have to be moved -->
+    <div class="menu">
+      <nuxt-link :to="$i18n.path('vignoble/bergerac')">
+        <el-button type="text">Bergerac</el-button>
+      </nuxt-link>
+      <nuxt-link :to="$i18n.path('vignoble/duras')">
+        <el-button type="text">Duras</el-button>
+      </nuxt-link>
+      <nuxt-link :to="$i18n.path('vignoble/monbazillac')">
+        <el-button type="text">Monbazillac</el-button>
+      </nuxt-link>
+      <nuxt-link :to="$i18n.path('vignoble/montravel')">
+        <el-button type="text">Montravel</el-button>
+      </nuxt-link>
+      <nuxt-link :to="$i18n.path('vignoble/pecharmant')">
+        <el-button type="text">Pécharmant</el-button>
+      </nuxt-link>
+      <nuxt-link :to="$i18n.path('vignoble/rosette')">
+        <el-button type="text">Rosette</el-button>
+      </nuxt-link>
+      <nuxt-link :to="$i18n.path('vignoble/saussignac')">
+        <el-button type="text">Saussignac</el-button>
+      </nuxt-link>
+    </div>
     <div class="lang-wrapper">
       <Lang />
     </div>
@@ -35,6 +59,15 @@ export default {
   &.floating {
     position: absolute;
     z-index: 10;
+  }
+}
+// Next selector is temporary
+.menu {
+  flex: 1;
+  display: flex;
+  justify-content: space-around;
+  .el-button {
+    margin-top: 0.4rem;
   }
 }
 .line {
