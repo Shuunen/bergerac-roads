cb30e6e5e9bab513f56def45d29242aebfb049ef
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-05-31 14:12:18 +0200

    cleaner import script  + now import lat/long

--- data/import.js
+++ data/import.js
@@ -4,50 +4,85 @@ const localApi = axios.create({
   timeout: 1000,
 })
 const isEqual = require('fast-deep-equal')
-const domains = require('./sample-domains.json').value
-
+const remoteDomains = require('./sample-domains.json').value
 let stopProcessing = false
 
-console.log('will check ' + domains.length + ' domains')
-domains.forEach(domain => {
-  if (stopProcessing) {
-    return
-  }
-  const id = domain.SyndicObjectID
-  const newData = {
-    id,
-    title: domain.SyndicObjectName,
+/**
+ * Convert large remote data into data that we will store in our db
+ * Look at sample-domains.json to see remote data structure
+ * @param {Domain} remote the domain data from remote API
+ */
+function remoteDomainToLocal(remote) {
+  return {
+    id: remote.SyndicObjectID,
+    title: remote.SyndicObjectName,
+    latitude: remote.GmapLatitude,
+    longitude: remote.GmapLongitude,
     active: true,
   }
-  localApi
+}
+
+function getLocalDomain(id) {
+  return localApi
     .get('/domains/' + id)
     .then(response => {
-      const localData = response.data
-      delete localData.updated // so we dont compare this
+      const localDomain = response.data
+      delete localDomain.updated // so we dont to compare or check this
       // console.log('domain with id', id, 'has been found')
-      if (!isEqual(localData, newData)) {
-        newData.updated = Date.now()
-        return localApi
-          .patch('/domains/' + id, newData)
-          .then(() => console.log(id, ': updated !'))
-      } else {
-        console.log(id, ': no updates')
-      }
+      return localDomain
     })
     .catch(error => {
       if (error.response && error.response.status === 404) {
-        localApi
-          .post('/domains', newData)
-          .then(() => {
-            console.log(id, ': created !')
-          })
-          .catch(error => {
-            console.error(error)
-            stopProcessing = true
-          })
+        return 'does-not-exists'
       } else {
         console.error(error)
         stopProcessing = true
       }
     })
-})
+}
+
+function addLocalDomain(data) {
+  return localApi
+    .post('/domains', data)
+    .then(() => {
+      console.log(data.id, ': freshly added !')
+    })
+    .catch(error => {
+      console.error(error)
+      stopProcessing = true
+    })
+}
+
+function patchLocalDomain(data) {
+  return localApi
+    .patch('/domains/' + data.id, data)
+    .then(() => console.log(data.id, ': updated !'))
+}
+
+function updateLocalDomain(remoteDomain) {
+  if (stopProcessing) {
+    return Promise.error('stop processing requested')
+  }
+  const newData = remoteDomainToLocal(remoteDomain)
+  return getLocalDomain(newData.id).then(response => {
+    if (response === 'does-not-exists') {
+      return addLocalDomain(newData)
+    }
+    const localData = response
+    if (!isEqual(localData, newData)) {
+      newData.updated = Date.now()
+      return patchLocalDomain(newData)
+    } else {
+      console.log(newData.id, ': no updates')
+    }
+  })
+}
+
+async function updateLocalDomains(remoteDomains) {
+  console.log('checking ' + remoteDomains.length + ' remote domains')
+  for (let i = 0; i < remoteDomains.length; i++) {
+    await updateLocalDomain(remoteDomains[i])
+  }
+}
+
+updateLocalDomains(remoteDomains)
