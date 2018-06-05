ae1cbd7a87ab2f157a8441fc39cc4ee83ad0cd50
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-06-05 19:08:38 +0200

    avoid logrocket on localhost

--- static/scripts/logrocket.js
+++ static/scripts/logrocket.js
@@ -1,15 +1,17 @@
-if (window.LogRocket) {
-  window.LogRocket.init('hipipp/bergerac-roads')
-  if (window.Rollbar) {
-    window.Rollbar.configure({
-      transform: function(obj) {
-        obj.sessionURL = window.LogRocket.sessionURL
-        return obj
-      },
-    })
+if (window.location.hostname !== 'localhost') {
+  if (window.LogRocket) {
+    window.LogRocket.init('hipipp/bergerac-roads')
+    if (window.Rollbar) {
+      window.Rollbar.configure({
+        transform: function(obj) {
+          obj.sessionURL = window.LogRocket.sessionURL
+          return obj
+        },
+      })
+    } else {
+      console.error('Rollbar not found, fail at connection with LogRocket')
+    }
   } else {
-    console.error('Rollbar not found, fail at connection with LogRocket')
+    console.error('LogRocket not found, cannot init')
   }
-} else {
-  console.error('LogRocket not found, cannot init')
 }
