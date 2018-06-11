bb247e943c592c7dd7aecf012881d9f52b74af06
LeGaby <leo.gaboriau@gmail.com>
2018-06-11 14:58:32 +0200

    Renamed itinerary component and added vue-multiselect package

--- pages/_lang/index.vue
+++ pages/_lang/index.vue
@@ -2,7 +2,7 @@
   <el-container direction="vertical" class="index">
     <Header />
     <HomeTop />
-    <Itinerary />
+    <Search />
     <LastDomains />
     <Footer />
   </el-container>
@@ -12,13 +12,13 @@
 import Header from '~/components/Header.vue'
 import Footer from '~/components/Footer.vue'
 import HomeTop from '~/components/Home-Top.vue'
-import Itinerary from '~/components/Itinerary.vue'
 import LastDomains from '~/components/Last-Domains.vue'
+import Search from '~/components/Search.vue'
 
 export default {
   components: {
     LastDomains,
-    Itinerary,
+    Search,
     HomeTop,
     Header,
     Footer,
