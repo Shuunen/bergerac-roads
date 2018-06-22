ecde2fa9031ce7026f8abf108b9b077ade6ca050
LeGaby <leo.gaboriau@gmail.com>
2018-06-22 13:38:35 +0200

    Improved select list's layout

--- components/Select-List.vue
+++ components/Select-List.vue
@@ -1,21 +1,6 @@
 <template>
   <div class="select-list-container" :style="{ height: `${height}px` }">
     <p class="introduction">{{ $t('search.introduction') }}</p>
-    <div class="autocomplete-input">
-      <el-input
-        ref="autocomplete"
-        :placeholder="$t('search.start')"
-        v-model="startingPoint"
-        clearable
-        @clear="setStartingPoint('')"
-      />
-      <el-button
-        icon="el-icon-view"
-        :disabled="checkedItems.length === 0"
-        @click="launchItineraryProcessing"
-        circle
-      />
-    </div>
     <el-checkbox-group
       v-model="checkedItems"
       @change="emitCheckedItems"
@@ -26,6 +11,19 @@
         :label="item.title"
       />
     </el-checkbox-group>
+    <el-input
+      ref="autocomplete"
+      :placeholder="$t('search.start')"
+      v-model="startingPoint"
+      clearable
+      @clear="setStartingPoint('')"
+    />
+    <el-button
+      :disabled="checkedItems.length === 0"
+      @click="launchItineraryProcessing"
+    >
+      {{ $t('search.itinerary') }}
+    </el-button>
   </div>
 </template>
 
@@ -104,22 +102,13 @@ export default {
   align-items: center;
   padding: 2rem;
   background-color: $white;
-  overflow-y: auto;
   .introduction {
     margin: 0 0 2rem;
     text-align: justify;
   }
-  .autocomplete-input {
-    width: 100%;
-    display: flex;
-    flex: none;
-    .el-button {
-      margin-left: 1rem;
-    }
-  }
   .el-checkbox-group {
     max-width: 100%;
-    margin-top: 2rem;
+    overflow-y: auto;
     .el-checkbox {
       display: block;
       .el-checkbox__label {
@@ -133,6 +122,12 @@ export default {
       margin-left: 0;
       margin-top: .5rem;
     }
+  }
+  .el-input {
+    margin: 2rem 0;
+  }
+  .el-button {
+    flex: none;
   }
 }
 </style>
