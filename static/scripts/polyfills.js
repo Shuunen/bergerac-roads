7ce9e3802b7dd103e0b5fbe2b22b65b0c1559f02
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-07-18 11:30:30 +0200

    added polyfills for PhantomJS

---
+++ static/scripts/polyfills.js
@@ -0,0 +1,14 @@
+
+console.log('in polyfills')
+
+if (!Object.entries) {
+  Object.entries = function(obj) {
+    var ownProps = Object.keys(obj),
+      i = ownProps.length,
+      resArray = new Array(i)
+    while (i--)
+      resArray[i] = [ownProps[i], obj[ownProps[i]]]
+
+    return resArray
+  }
+}
