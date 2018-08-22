406a6f9630c07a998a670272cc407b0402a1a15e
Groleau.b <groleau.b@gmail.com>
2018-08-22 16:15:06 +0200

    Add BG Key for API

--- plugins/google-maps.js
+++ plugins/google-maps.js
@@ -3,7 +3,7 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyAGbKkax4IiRllXcrtmy3Og-b7ljNTiRpc',
+    key: 'AIzaSyBGK7UtG1tDdx6pYcDPzT7EwpCy6KDQXSA',
     libraries: 'places', // This is required if you use the Autocomplete plugin
     // OR: libraries: 'places,drawing'
     // OR: libraries: 'places,drawing,visualization'
