213dc01eb8cdcf9f40a2cde1fb0073cfa1aec93d
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-10-21 19:38:56 +0200

    domain styling WIP #45

--- pages/_lang/domaine/_id.vue
+++ pages/_lang/domaine/_id.vue
@@ -6,52 +6,44 @@
 
 
     <el-container>
-      <el-aside class="aside">
 
-        <el-card class="box-card">
-          <span><i class="el-icon-info"/>  Infos</span>
-          <p v-if="data.mail" ><i class="el-icon-message"/> {{ data.mail }}</p>
-
-          <ul v-if="data.phones">
-            <li v-for="phone in data.phones" :key="phone" ><i class="el-icon-phone"/> {{ phone }} </li>
-          </ul>
-
-          <p>{{ data.adress }}<br><i class="el-icon-location-outline"/>{{ data.town }}</p>
-
-          <p v-if="data.websites && data.websites.length"><strong>Site web</strong> : {{ data.websites.join(', ') }}</p>
-
-          <a v-if="data.socialFacebook" :href="data.socialFacebook">
-            <img alt="facebook" src="http://www.vins-bergeracduras.fr/wp-content/themes/vins-de-bergerac/images/icone_facebook.png">
-          </a>
-
-        </el-card>
-
-        <p v-if="data.vineyards && data.vineyards.length"><strong>Cépages</strong> : {{ data.vineyards.join(', ') }}</p>
-
-        <p v-if="data.langs && data.langs.length"><strong>Langues parlées</strong> : {{ data.langs.join(', ') }}</p>
-
-        <p v-if="data.services && data.services.length"><strong>Services</strong> : {{ data.services.join(', ') }}</p>
-
-        <p v-if="data.statuses && data.statuses.length"><strong>Status</strong> : {{ data.statuses.join(', ') }}</p>
-
-      </el-aside>
       <el-main>
         <div class="encart" v-loading="loading">
           <div class="line">
             <h1 class="title">{{ data.title }}</h1>
           </div>
 
+          <div class="line quick-actions">
+            <el-button v-if="data.mail" @click="sendMail" icon="el-icon-message">{{ data.mail }}</el-button>
+            <el-button v-show="data.phones && data.phones.length" v-for="phone in data.phones" :key="phone" icon="el-icon-phone-outline">{{ phone }}</el-button>
+          </div>
+
+          <p v-if="data.websites && data.websites.length"><strong>Site web</strong>&nbsp;: {{ data.websites.join(', ') }}</p>
+          <!--
+          <p>{{ data.adress }}<br><i class="el-icon-location-outline"/>{{ data.town }}</p>
+
+
+          <a v-if="data.socialFacebook" :href="data.socialFacebook">
+            <img alt="facebook" src="http://www.vins-bergeracduras.fr/wp-content/themes/vins-de-bergerac/images/icone_facebook.png">
+          </a>
+
           <el-row :gutter="20">
             <el-tag v-for="tag in data.tags" :key="tag" type="info" size="medium">
               {{ tag }}
             </el-tag>
           </el-row>
+          -->
 
-          <span class="glasses line">
+          <div class="glasses line start">
+            <strong>Cépages</strong>&nbsp;:
             <div class="icon glass" v-for="(wine, index) in wines" :key="index" :class="wine" />
-          </span>
+          </div>
 
-          <el-container direction="vertical" v-if="!data.message">
+          <div class="separator line">
+            <i class="el-icon-more-outline" />
+          </div>
+
+          <el-container direction="vertical" v-if="!data.message" class="description">
             <p v-if="$i18n.locale === 'fr'">{{ data.description }} </p>
             <p v-if="$i18n.locale === 'en'">{{ data.descriptionEn }} </p>
           </el-container>
@@ -90,7 +82,7 @@ export default {
     }
   },
   computed: {
-    image: function() {
+    image() {
       let path = process.env.cdn + '/images/pixabay/bouchon-01.jpg'
       if (!this.data) {
         console.warn('data not available, using default image...')
@@ -101,11 +93,10 @@ export default {
       } else {
         console.warn('image not available, using default one...')
       }
-      // path =  path
       path = path.replace('cdn/none', 'height/' + 400)
       return path
     },
-    added: function() {
+    added() {
       if (!this.data.updated) {
         return null
       }
@@ -123,7 +114,7 @@ export default {
       const added = new Intl.DateTimeFormat('fr-FR', options).format(date)
       return added
     },
-    wines: function() {
+    wines() {
       let wines = []
       if (this.data.tags && this.data.tags.length) {
         winesToDisplay.forEach(wine => {
@@ -159,11 +150,17 @@ export default {
       } else {
         this.loading = false
         this.data = {
-          title: 'Domaine inconnu',
-          message: 'Désolé mais ce domaine n\'a pas été trouvé.',
+          title: this.$t('domain.unknownTitle'),
+          message: this.$t('domain.unknownMessage'),
         }
         this.backgroundStyle = { backgroundImage: 'url(' + this.image + ')' }
       }
+    },
+    sendMail() {
+      const mail = this.data.mail
+      const subject = this.$t('domain.sendMailSubject')
+      const body = this.$t('domain.sendMailBody')
+      window.location.href = `mailto:${mail}?subject=${subject}&body=${body}`
     },
   },
 }
@@ -206,5 +203,21 @@ export default {
   color: white;
   font-size: 201%;
   text-align: center;
+}
+.quick-actions {
+  padding-bottom: 20px;
+}
+.separator {
+  padding-bottom: 10px;
+  font-size: 200%;
+}
+.description {
+  line-height: 2;
+  & > p::first-letter {
+    font-size: 200%;
+    margin-right: 5px;
+    color: $red-d2;
+    line-height: 1;
+  }
 }
 </style>
