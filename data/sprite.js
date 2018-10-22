2b6ed4fec6a6dc0f439de30f9e87a01b37d00e16
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-10-22 11:05:46 +0200

    fixed sprite creation

--- data/sprite.js
+++ data/sprite.js
@@ -1,50 +1,59 @@
 import nsg from 'node-sprite-generator'
+import isInstalled from 'is-installed'
 
-/*
-nsg({
-  compositor: 'jimp', // jimp
-  compositorOptions: {
-    compressionLevel: 6
-  },
-  src: [
-    'assets/icons/**//* \.png',
-  ],
-  layout: 'packed', // packed
-  spritePath: 'assets/images/icons.png',
-  stylesheet: 'scss',
-  stylesheetPath: 'assets/styles/ressources/icons-transparent.scss',
-  stylesheetOptions: {
-    prefix:'transparent-'
-  }
-}, function(err) {
-  if (err) {
-    console.log('Sprite PNG failed !', err)
-  } else {
-    console.log('Sprite PNG generated!')
-  }
-})
-*/
+function createPngSprite() {
+  nsg({
+    compositor: 'jimp', // jimp
+    compositorOptions: { compressionLevel: 6 },
+    src: ['assets/icons/**/*.png'],
+    layout: 'packed',
+    spritePath: 'assets/images/icons.png',
+    stylesheet: 'scss',
+    stylesheetPath: 'assets/styles/ressources/icons-transparent.scss',
+    stylesheetOptions: { prefix: 'transparent-' }
+  }, function (err) {
+    if (err) {
+      console.log('Sprite PNG failed !', err)
+    } else {
+      showHints('png')
+    }
+  })
+}
 
-nsg({
-  compositor: 'gm', // jimp
-  compositorOptions: {
-    compressionLevel: 7
-  },
-  src: [
-    'assets/icons/**/*.jpg',
-  ],
-  layout: 'packed', // packed
-  spritePath: 'assets/images/icons.jpg',
-  stylesheet: 'scss',
-  stylesheetPath: 'assets/styles/ressources/icons.scss'
-}, function(err) {
-  if (err) {
-    console.log('Sprite JPG failed !', err)
+function showHints(type) {
+  console.log(`Sprite ${type} generated!`)
+  console.log('TODO : Upload sprite to CDN')
+  console.log(`TODO : Background should target "#{$cdn}/images/icons.${type}" in icons${type === 'jpg' ? '' : '-transparent'}.scss`)
+  console.log('TODO : Specify "background-size: 1234px;" under "@mixin sprite($sprite)"')
+  console.log('TODO : Invalidate https://admin-v3.cloudimg.io/admin/project/invalidation')
+  console.log(`URL : /cdn/none/none/bergerac.lebowsky-dev.xyz/images/icons.${type}`)
+}
+
+function createJpgSprite() {
+  nsg({
+    compositor: 'gm', // jimp
+    compositorOptions: { compressionLevel: 7 },
+    src: ['assets/icons/**/*.jpg'],
+    layout: 'packed',
+    spritePath: 'assets/images/icons.jpg',
+    stylesheet: 'scss',
+    stylesheetPath: 'assets/styles/ressources/icons.scss'
+  }, function (err) {
+    if (err) {
+      console.log('Sprite JPG failed !', err)
+    } else {
+      showHints('jpg')
+    }
+  })
+}
+
+function startJpgSprite() {
+  if (isInstalled.sync('gm')) {
+    createJpgSprite()
   } else {
-    console.log('Sprite JPG generated!')
-    console.log('TODO : Upload sprite to CDN')
-    console.log('TODO : Background should target "#{$cdn}/images/icons.jpg" in icons.scss')
-    console.log('TODO : Invalidate https://admin-v3.cloudimg.io/admin/project/invalidation')
-    console.log('URL : /cdn/none/none/bergerac.lebowsky-dev.xyz/images/icons.jpg')
+    console.log('Error : ImageMagick/GraphicsMagick needs to be installed when using the gm compositor')
   }
-})
+}
+
+startJpgSprite()
+createPngSprite()
