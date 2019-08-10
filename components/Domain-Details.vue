3e8785e3d6347e860123462620dd5dfe3f4aede1
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-08-10 19:41:00 +0200

    add domain details component

---
+++ components/Domain-Details.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class="domain-details" :class="['theme-' + theme]">
+    <div class="line">
+      <h1 class="title">{{ domain.title }}</h1>
+    </div>
+
+    <div class="line quick-actions">
+      <el-button v-if="domain.mail" @click="sendMail(domain.mail)" icon="el-icon-message">{{ domain.mail }}</el-button>
+      <el-button v-show="domain.phones && domain.phones.length" v-for="phone in domain.phones" :key="phone" icon="el-icon-phone-outline">{{ phone }}</el-button>
+    </div>
+
+    <p v-if="domain.websites && domain.websites.length" class="websites hide-for-modal">
+      <strong>Site web :</strong>
+      <a :href="url" v-for="url in domain.websites" :key="url">
+        {{ url }}
+        <span class="comma">,&nbsp;</span>
+      </a>
+    </p>
+
+    <!--
+    <p>{{ domain.address }}<br><i class="el-icon-location-outline"/>{{ domain.town }}</p>
+
+    <a v-if="domain.socialFacebook" :href="domain.socialFacebook">
+      <img alt="facebook" src="http://www.vins-bergeracduras.fr/wp-content/themes/vins-de-bergerac/images/icone_facebook.png">
+    </a>
+
+    <el-row :gutter="20">
+      <el-tag v-for="tag in domain.tags" :key="tag" type="info" size="medium">
+        {{ tag }}
+      </el-tag>
+    </el-row>
+    -->
+
+    <div class="glasses line start">
+      <strong class="hide-for-modal">Cépages&nbsp;:</strong>
+      <div class="wine" v-for="(wine, index) in wines" :key="index">
+        <div class="text hide-for-modal" :class="wine">{{ readable(wine) }}</div>
+        <div class="icon glass" :class="wine" />
+      </div>
+    </div>
+
+    <div class="separator line hide-for-modal">
+      <i class="el-icon-more-outline" />
+    </div>
+
+    <el-container direction="vertical" v-if="!domain.message" class="description">
+      <p v-if="$i18n.locale === 'fr'">{{ domain.description }}</p>
+      <p v-if="$i18n.locale === 'en'">{{ domain.descriptionEn }}</p>
+    </el-container>
+
+    <el-alert v-if="domain.message" :title="domain.message" center :closable="false" type="warning" show-icon />
+
+    <div class="col hide-for-modal">
+      <nuxt-link :to="$i18n.path('')">
+        <el-button icon="el-icon-arrow-left" class="back">{{ $t('common.back-home') }}</el-button>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+const winesDisplayOrder = ['blanc-sec', 'blanc-moelleux', 'blanc-liquoreux', 'rose', 'rouge']
+
+export default {
+  props: {
+    domain: {
+      type: Object,
+      default: null,
+      required: true,
+    },
+    theme: {
+      type: String,
+      default: 'modal',
+    },
+  },
+  data () {
+    return {
+      wines: [],
+    }
+  },
+  mounted () {
+    this.setWines()
+  },
+  methods: {
+    setWines () {
+      if (!this.domain.tags || !this.domain.tags.length) {
+        return console.error('this domain has no wines tags', this.domain)
+      }
+      winesDisplayOrder.forEach((wine) => {
+        const tag = 'vin-' + wine
+        if (this.domain.tags.includes(tag)) {
+          this.wines.push(tag)
+        }
+      })
+    },
+    readable (wineTag) {
+      switch (wineTag) {
+        case 'vin-blanc-sec':
+          return 'Blanc sec'
+        case 'vin-blanc-moelleux':
+          return 'Blanc moelleux'
+        case 'vin-blanc-liquoreux':
+          return 'Blanc liquoreux'
+        case 'vin-rose':
+          return 'Rosé'
+        case 'vin-rouge':
+          return 'Rouge'
+        default:
+          return wineTag
+      }
+    },
+    sendMail (to) {
+      const subject = this.$t('domain.sendMailSubject')
+      const body = this.$t('domain.sendMailBody')
+      window.location.href = `mailto:${to}?subject=${subject}&body=${body}`
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/styles/ressources/variables.scss";
+
+.domain-details {
+  .title {
+    color: $red-d2;
+    text-align: center;
+    word-break: break-word;
+  }
+  .websites a:last-child .comma {
+    display: none;
+  }
+  .wine {
+    display: flex;
+    margin: 5px;
+    .text {
+      border-bottom: 2px dotted;
+      &.vin-blanc-sec {
+        border-color: $blanc;
+      }
+      &.vin-blanc-moelleux {
+        border-color: $moelleux;
+      }
+      &.vin-blanc-liquoreux {
+        border-color: $liquoreux;
+      }
+      &.vin-rose {
+        border-color: $rose;
+      }
+      &.vin-rouge {
+        border-color: $rouge;
+      }
+    }
+  }
+  &.theme-modal {
+    .title {
+      font-size: 250%;
+      margin-top: 10px;
+    }
+    .glasses {
+      justify-content: center;
+    }
+    .hide-for-modal {
+      display: none;
+    }
+  }
+}
+</style>
