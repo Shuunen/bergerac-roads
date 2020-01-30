1c0379ea257f9172fc8b81d6ee1ff1aa13584b54
b.groleau <groleau.b@gmail.com>
2020-01-30 09:56:50 +0100

    Add charter page trad

--- pages/_lang/charter.vue
+++ pages/_lang/charter.vue
@@ -9,11 +9,15 @@
           <h1 class="title">{{ $t('charter.title') }}</h1>
         </div>
         <el-container direction="vertical">
-          <p>Lorem ipsum enim quisque sibi plurimum confidit et ut quisque maxime virtute et sapientia sic munitus est, ut nullo egeat suaque omnia in se ipso posita iudicet, ita in amicitiis expetendis colendisque maxime excellit. Quid enim? Africanus indigens mei? Minime hercule! ac ne ego quidem illius; sed ego admiratione quadam virtutis eius, ille vicissim opinione fortasse non nulla, quam de meis moribus habebat, me dilexit; auxit benevolentiam consuetudo. Sed quamquam utilitates multae et magnae consecutae sunt, non sunt tamen ab earum spe causae diligendi profectae.</p>
-          <p>Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.</p>
-          <p>Superatis Tauri montis verticibus qui ad solis ortum sublimius attolluntur, Cilicia spatiis porrigitur late distentis dives bonis omnibus terra, eiusque lateri dextro adnexa Isauria, pari sorte uberi palmite viget et frugibus minutis, quam mediam navigabile flumen Calycadnus interscindit.</p>
-          <p>Hac ex causa conlaticia stipe Valerius humatur ille Publicola et subsidiis amicorum mariti inops cum liberis uxor alitur Reguli et dotatur ex aerario filia Scipionis, cum nobilitas florem adultae virginis diuturnum absentia pauperis erubesceret patris.</p>
-          <p>Siquis enim militarium vel honoratorum aut nobilis inter suos rumore tenus esset insimulatus fovisse partes hostiles, iniecto onere catenarum in modum beluae trahebatur et inimico urgente vel nullo, quasi sufficiente hoc solo, quod nominatus esset aut delatus aut postulatus, capite vel multatione bonorum aut insulari solitudine damnabatur.</p>
+            <h2 class="title">{{ $t('charter.subtitle-1') }}</h2>
+          <p>{{ $t('charter.body-1-1') }}</p>
+          <p>{{ $t('charter.body-1-2') }}</p>
+          <h2 class="title">{{ $t('charter.subtitle-2') }}</h2>
+           <ul>
+              <li v-for="(element, index) in $t(`charter.commitments`)" :key="index">{{ element }}</li>
+            </ul>
+            <h2 class="title">{{ $t('charter.subtitle-3') }}</h2>
+            <p>{{ $t('charter.body-3-1') }}</p>
         </el-container>
         <div class="col">
           <nuxt-link :to="$i18n.path('')">
